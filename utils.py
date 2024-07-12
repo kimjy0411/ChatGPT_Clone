aec9229b22033ab051cdca09925b8a5bf2322c28
kim jae young <kbs00717@gmail.com>
2024-07-12 21:30:27 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,15 @@
+from langchain_core.callbacks.base import BaseCallbackHandler
 import streamlit as st
+
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container, initial_text=""):
+        self.container = container
+        self.text = initial_text
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.text += token
+        self.container.markdown(self.text)
+
 
 
 def print_messages():
